a7f8698c474cd1bb1f09f944627e51f27707be9e
Alessandra <als.sanches@outlook.com>
2024-08-09 22:50:10 -0300

    Primeiro commit

---
+++ pages/FAQ.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link"
+
+export default function FAQPage() {
+    return (
+      <div className="FAQ">
+        <h1>Alura cases - Página FAQ </h1>
+        <Link href= "/"> Ir para a página HomePage </Link>
+      </div>
+    )
+  }
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link"
+
+export default function homepage() {
+  return (
+    <div className="FAQ">
+      <h1>Alura cases - HomePage </h1>
+      <Link href="/FAQ">Ir para a página do FAQ</Link>
+    </div>
+  )
+}
